88a4c3de02206ed8a000f6a0cfc325c39895f47c
mccleskeyc <mccleskeyc@gmail.com>
2021-02-24 11:57:58 -0500

    commit tree a3ef4cd2e6dd956bcb186394de66087ba7c4119c

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -8,7 +10,7 @@ function renderBooks(books) {
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
     main.appendChild(h2)
-  })
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
